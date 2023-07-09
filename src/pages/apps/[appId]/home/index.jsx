e74bfa4ca53c959c7f03eba6284feb31ea1b8e60
vsidera <siderravictor@gmail.com>
2023-07-09 14:55:57 +0300

    consume apis for dashboard

--- src/pages/apps/[appId]/home/index.jsx
+++ src/pages/apps/[appId]/home/index.jsx
@@ -6,19 +6,92 @@ import axios from "axios";
 import { FormatQuote } from "@mui/icons-material";
 import { useRouter } from "next/router";
 import DatePickerValue from "../../../../components/utils/datePicker";
+import { authHeaders } from "../../../api/utils/headers/headers";
 
 const Home = () => {
   const router = useRouter();
+
+  const config = authHeaders();
+
+  const [sent, setSent] = useState(1000);
+  const [failed, setFailed] = useState(0);
+  const [sentLastWeek, setSentLastWeek] = useState(0);
+  const [failedLastWeek, setFailedLastWeek] = useState(0);
+  const [sentPercentageChange, setSentPercentageChange] = useState(0);
+  const [failedPercentageChange, setFailedPercentageChange] = useState(0);
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://suss-ads.zohari.tech/api/v1/analytics/weekcompare/1",
+          config
+        );
+        // const analyticsData = response.data;
+
+        const analyticsData = [
+          {
+              "Description": "Message failed",
+              "ThisWeekCount": 100,
+              "LastWeekCount": 50
+          },
+          {
+            "Description": "Message sent",
+            "ThisWeekCount": 2000,
+            "LastWeekCount": 500
+        }
+      ]
+
+        // Calculate the total counts for 'sent' and 'failed' for this week and last week
+        let sentCount = 0;
+        let failedCount = 0;
+        let sentCountLastWeek = 0;
+        let failedCountLastWeek = 0;
+
+        analyticsData.forEach((item) => {
+          if (item.Description === "Message sent") {
+            sentCount += item.ThisWeekCount;
+            sentCountLastWeek += item.LastWeekCount;
+          } else if (item.Description === "Message failed") {
+            failedCount += item.ThisWeekCount;
+            failedCountLastWeek += item.LastWeekCount;
+          }
+        });
+
+        setSent(sentCount);
+        setFailed(failedCount);
+        setSentLastWeek(sentCountLastWeek);
+        setFailedLastWeek(failedCountLastWeek);
+
+        // Calculate the percentage change for 'sent' and 'failed'
+        const sentChange = sentCount - sentCountLastWeek;
+        const failedChange = failedCount - failedCountLastWeek;
+
+        const sentPercentageChange = (sentChange / sentCountLastWeek) * 100;
+        const failedPercentageChange =
+          (failedChange / failedCountLastWeek) * 100;
+
+        setSentPercentageChange(sentPercentageChange);
+        setFailedPercentageChange(failedPercentageChange);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const quotes = [
     "Create personalized two-way messaging campaigns which bring out experiences that drive conversions.",
     "Target different customer groups and send them SMSs on the latest and offerings not be missed.",
     "Send important notifications and triggers through SMS.",
     "Automate reminders on renewals, payments, next appointments, and other repetitive future events.",
-    "Generate leads, upsell, and cross-sell with a low running cost and higher ROI platform as compared to other traditional forms of advertisement."
+    "Generate leads, upsell, and cross-sell with a low running cost and higher ROI platform as compared to other traditional forms of advertisement.",
     // Add more quotes as needed
   ];
-  
+
   const getRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     return quotes[randomIndex];
@@ -32,11 +105,10 @@ const Home = () => {
       <div className="m-16">
         <h2 className="mt-4 text-xl font-semibold">Dashboard</h2>
         <div className="flex justify-end">
-  {/* <div>
+          {/* <div>
     <DatePickerValue />
   </div> */}
-</div>
-        
+        </div>
 
         <Grid container spacing={4} className="mt-8">
           <Grid item xs={12} md={4} lg={4}>
@@ -45,10 +117,12 @@ const Home = () => {
               <div className="flex items-center mb-4">
                 <FormatQuote className="text-[#094C95] mr-2" />
                 <p className="m-16 text-2xl font-bold text-[#094C95]">
-                {getRandomQuote()}
+                  {getRandomQuote()}
                 </p>
               </div>
-              <p className="text-xs mt-1 text-gray-500">Boost Your Business With An Easy-To-Use SMS Service.</p>
+              <p className="text-xs mt-1 text-gray-500">
+                Boost Your Business With An Easy-To-Use SMS Service.
+              </p>
             </div>
           </Grid>
 
@@ -62,7 +136,6 @@ const Home = () => {
                     <Image
                       src="/images/image2.avif"
                       alt="Image"
-                      layout="responsive"
                       width={500}
                       height={300}
                     />
@@ -70,8 +143,11 @@ const Home = () => {
                   <div className="w-2/3 pl-4">
                     {/* Add your text content */}
                     <h3 className="text-md font-medium mt-4">Messages sent</h3>
-                    <p className="text-lg mt-1">150,000</p>
-                    <p className="mt-1 text-green-500">+15.55%</p>
+                    <p className="text-lg mt-1">{sent}</p>
+                    <p className={sentPercentageChange >= 0 ? "mt-1 text-green-500" : "mt-1 text-red-500"}>
+                      {sentPercentageChange.toFixed(1)}%
+                    </p>
+
                     <p className="mt-1 text-xs text-gray-500">
                       Since last week
                     </p>
@@ -93,10 +169,12 @@ const Home = () => {
                   <div className="w-2/3 pl-4">
                     {/* Add your text content */}
                     <h3 className="text-md font-medium mt-4">
-                      Messages delivered
+                      Messages Failed
                     </h3>
-                    <p className="text-lg mt-1">141,000</p>
-                    <p className="mt-1 text-red-500">-10%</p>
+                    <p className="text-lg mt-1">{failed}</p>
+                    <p className={failedPercentageChange >= 0 ? "mt-1 text-red-500" : "mt-1 text-green-500"}>
+                      {failedPercentageChange.toFixed(1)}%
+                    </p>
                     <p className="text-xs mt-1 text-gray-500">
                       Since last week
                     </p>
