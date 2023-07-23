a7be29cdab06d4a15f7815a8e8fb08bdc8382b71
vsidera <siderravictor@gmail.com>
2023-07-23 07:04:27 +0300

    api units exp by month

---
+++ src/pages/api/actions/analytics/analyticsAction.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import apiUrl from "../../utils/apiUtils/apiUrl";
+import { authHeaders } from '../../../api/utils/headers/headers';
+
+export function unitsExpenditure(formValues) {
+    const appsUrl = `${apiUrl.UNITS_EXPENDITURE}/${formValues.app_id}`;
+    const config = authHeaders();
+  
+    return axios
+      .get(appsUrl, config)
+      .then((res) => {
+      
+        if (res.data && res.status === 200) {
+
+            console.log("THE RESPONSE IS !!!!!!!",res)
+          
+        }
+        return res;
+      })
+      .catch((error) => {
+        if (error.response) {
+        
+          return {
+            errors: {
+              _error: 'The contacts could not be returned.',
+            },
+          };
+        }   
+        return {
+          errors: {
+            _error: 'Network error. Please try again.',
+          },
+        };
+      });
+  }
