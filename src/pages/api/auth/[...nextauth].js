1d6414c7325d49365dff8526215ad857763d365b
vsidera <siderravictor@gmail.com>
2023-11-22 14:01:52 +0300

    hard code urls

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,27 +4,27 @@ import Auth0Provider from 'next-auth/providers/auth0';
 export default NextAuth({
   providers: [
     Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER_BASE_URL,
-      redirectUri: process.env.AUTH0_REDIRECT_URI,
+      clientId: 'ojwD0fy1h9KkTwFcpy5GozidovRNSPyX',
+      clientSecret: 'CNNd4W_6e_VewDQr9CdnIx_jUA0-sz3rwOISRh0n7a9VBnjytAPxxxXt1jpDedhq',
+      issuer: 'https://suss.us.auth0.com',
+      redirectUri: 'https://new-suss-staging.vercel.app/api/auth/callback/auth0',
       idToken: true,
       token: {
         params: {
-          audience: process.env.AUTH0_AUDIENCE
+          audience: 'https://suss.us.auth0.com/api/v2/'
         }
       },
       authorization: {
         params: {
-          audience: encodeURI(process.env.AUTH0_AUDIENCE)
+          audience: encodeURI('https://suss.us.auth0.com/api/v2/')
         }
       }
     }),
   ],
-  secret: process.env.AUTH0_SECRET,
+  secret: '3064e3c73fd940fb24bf86e0199928ed953aa489539e107cf48ce45bc3bd2024',
   session: {
     strategy: 'jwt',
-    secret: process.env.NEXTAUTH_SECRET
+    secret: '3064e3c73fd940fb24bf86e0199928ed953aa489539e107cf48ce45bc3bd2024'
   },
   jwt: {},
   callbacks: {
@@ -37,9 +37,9 @@ export default NextAuth({
     },
     async redirect(url) {
       if (typeof url === 'string') {
-        return url.startsWith(process.env.AUTH0_BASE_URL) ? url : process.env.AUTH0_BASE_URL;
+        return url.startsWith('https://new-suss-staging.vercel.app') ? url : 'https://new-suss-staging.vercel.app';
       } else {
-        return process.env.AUTH0_BASE_URL;
+        return 'https://new-suss-staging.vercel.app';
       }
     },
     async jwt({ token, account }) {
@@ -52,7 +52,8 @@ export default NextAuth({
       if (profile?.account?.access_token) { // Check if the token exists in the account object
         // console.log('ACCESS TOKEN!!!!!!!:', profile.account.access_token); // Log the token to the console
         console.log('ENVIRONMENTS!!!!!!!:', process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID)
-        return '/apps';
+        // return '/apps';
+        return true;
       }
       return true;
     },
