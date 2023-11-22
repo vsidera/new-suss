51ea67630ad75f27c70c24234a8cbd0e3019a585
vsidera <siderravictor@gmail.com>
2023-11-22 14:23:20 +0300

    console log token

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -50,10 +50,10 @@ export default NextAuth({
     },
     async signIn( profile) {
       if (profile?.account?.access_token) { // Check if the token exists in the account object
-        // console.log('ACCESS TOKEN!!!!!!!:', profile.account.access_token); // Log the token to the console
-        console.log('ENVIRONMENTS!!!!!!!:', process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID)
-        // return '/apps';
-        return true;
+        console.log('ACCESS TOKEN!!!!!!!:', profile.account.access_token); // Log the token to the console
+        // console.log('ENVIRONMENTS!!!!!!!:', process.env.AUTH0_CLIENT_ID)
+        return '/apps';
+        // return true;
       }
       return true;
     },
